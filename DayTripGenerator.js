2c3ee796f32737e404f6794b6860e8827a003762
andrew alton <joeblk@gmail.com>
2021-09-05 12:50:05 -0800

    Compiled aspects of project under single function for more accessibility elswhere if needed

--- DayTripGenerator.js
+++ DayTripGenerator.js
@@ -17,48 +17,53 @@ function getRandomTrip(arrayToGetSelectionFrom){
     return arrayToGetSelectionFrom[selectionNumber];
 }
 
-let destination = ["Fiji", "Belize", "Sydney", "Cabo San Lucas", "New Zealand"];
-let randomDestination = getRandomTrip(destination)
-console.log(randomDestination);  
-//
-let restaurant = ["Burger King", "Taco Bell", "Popeye's", "Fudruckers", "Smashburger"];
-let randomRestaurant = getRandomTrip(restaurant);
-console.log(randomRestaurant);
-//
-let transportation = ["Plane", "Boat", "Teleportation", "Submarine", "Jet Pack"];
-let randomTransportation = getRandomTrip(transportation);
-console.log(randomTransportation);
-//
-let entertainment = ["Juggling", "Fire Eating", "Underwater Basket Weaving", "Bearded Lady Opera", "Gladitorial Combat"];
-let randomEntertainment = getRandomTrip(entertainment);
-console.log(randomEntertainment);
-
-let userInput = prompt("Are you happy with your trip and ready to go?");
-while(userInput === "no") {
-   randomDestination = getRandomTrip(destination);
+function generateDayTrip(){
+   let destination = ["Fiji", "Belize", "Sydney", "Cabo San Lucas", "New Zealand"];
+   let randomDestination = getRandomTrip(destination)
+   console.log(randomDestination);  
+   //
+   let restaurant = ["Burger King", "Taco Bell", "Popeye's", "Fudruckers", "Smashburger"];
+   let randomRestaurant = getRandomTrip(restaurant);
+   console.log(randomRestaurant);
+   //
+   let transportation = ["Plane", "Boat", "Teleportation", "Submarine", "Jet Pack"];
+   let randomTransportation = getRandomTrip(transportation);
+   console.log(randomTransportation);
+   //
+   let entertainment = ["Juggling", "Fire Eating", "Underwater Basket Weaving", "Bearded Lady Opera", "Gladitorial Combat"];
+   let randomEntertainment = getRandomTrip(entertainment);
+   console.log(randomEntertainment);
+   
+   let userInput = prompt("Are you happy with your trip and ready to go?");
+   while(userInput === "no") {
+       randomDestination = getRandomTrip(destination);
+         console.log(randomDestination);
+       randomRestaurant = getRandomTrip(restaurant);
+         console.log( randomRestaurant);
+       randomTransportation = getRandomTrip(transportation);
+         console.log(randomTransportation);
+       randomEntertainment = getRandomTrip(entertainment);
+         console.log(randomEntertainment);
+       userInput = "yes";
+   } 
+   console.log("Trip Complete");
+    userInput = prompt("Are you happy with your trip now?");
+    if(userInput === "no"){
+    getRandomTrip(destination);
     console.log(randomDestination);
-    randomRestaurant = getRandomTrip(restaurant);
+    getRandomTrip(restaurant);
     console.log( randomRestaurant);
-    randomTransportation = getRandomTrip(transportation);
+    getRandomTrip(transportation);
     console.log(randomTransportation);
-    randomEntertainment = getRandomTrip(entertainment);
-    console.log(randomEntertainment);
-    userInput = "yes";
-} 
-console.log("Trip Complete");
- userInput = prompt("Are you happy with your trip now?");
- if(userInput === "no"){
- getRandomTrip(destination);
- console.log(randomDestination);
- getRandomTrip(restaurant);
- console.log( randomRestaurant);
- getRandomTrip(transportation);
- console.log(randomTransportation);
- getRandomTrip(entertainment);
- console.log(randomEntertainment); 
- alert("Okay, maybe we can work out a trip for you some other time")
- userInput = "no"
-} else {
-   console.log("Trip Complete")
- }
+    getRandomTrip(entertainment);
+    console.log(randomEntertainment); 
+    alert("Okay, maybe we can work out a trip for you some other time")
+    userInput = "no"
+   } else {
+      console.log("Trip Complete")
+    }
+   
+   
+}
 
+generateDayTrip();
